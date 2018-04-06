1ea7427941b4dea2a570c2d8c5036e13a2d168e0
macluckie dimitri <macluckie@hotmail.fr>
2018-04-06 12:02:06 +0200

    interface ok

--- GamePlay.php
+++ GamePlay.php
@@ -5,7 +5,7 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-      
+
     integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
 
@@ -19,8 +19,41 @@ integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6J
 	</head>
 
 	<body>
-		<h1></h1>
+		<h1>FIGHT</h1>
+<div class="flip3Dplayer1">
 
+  <div class="back">
+    <form method="post">
+      <input type="submit" name="ATTACKPLAYER2" value="ATTACKPLAYER2"/>
+        <input type="submit" name="POTION" value="POTION"/>
+    </form>
+      <p>Name: </p>
+      <p> Life: </p>
+      <p>intelligence: </p>
+    </div>
+    <div class="front">
+
+    </div>
+</div>
+
+
+
+<div class="flip3Dplayer2">
+  <div class="back">
+    <form method="post">
+      <input type="submit" name="ATTACKPLAYER1" value="ATTACKPLAYER1"/>
+        <input type="submit" name="POTION" value="POTION"/>
+    </form>
+    <p>Name: </p>
+    <p> Life: </p>
+    <p>intelligence: </p>
+  </div>
+
+  <div class="front">
+
+  </div>
+
+</div>
 
 
 
